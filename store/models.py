37177b135c28019b9b32a879e28cdc1bce078c21
Vladi_bg <vladikomputers2000@abv.bg>
2023-07-20 10:01:41 +0300

    Added images and update models.py file

--- store/models.py
+++ store/models.py
@@ -16,18 +16,18 @@ class ProductsModel(models.Model):
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True)
 
-    # image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
-    #
-    # @property
-    # def imageURL(self):
-    #     try:
-    #         url = self.image.url
-    #     except:
-    #         url = ''
-    #     return url
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class OrderModel(models.Model):
--- store/views.py
+++ store/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import (CustomerModel,
+                     ProductsModel,
+                     OrderModel,
+                     OrderItemModel,
+                     ShippingAddressModel)
 
 
 def store(request):
-    context = {}
+    products = ProductsModel.objects.all()
+    context = {
+        'products': products
+    }
 
     return render(request, 'store/store.html', context)
 
